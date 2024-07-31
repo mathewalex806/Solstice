f83a84de3a8db8e93230b91404a2f1664c7fd3bf
Alex <mathewalex806@gmail.com>
2024-07-31 17:18:46 +0530

    added login function

--- Solstice/app1/views.py
+++ Solstice/app1/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
 
 
 @api_view(["POST","GET"])
@@ -19,3 +21,21 @@ def signup(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response({"message":"Get url for the signup url"},status=status.HTTP_200_OK)
+
+## Adding the login fuction
+
+@api_view(["GET","POST"])
+@permission_classes([AllowAny])
+def login(request):
+    if request.method == "POST":
+        username = request.data["username"]
+        password = request.data["password"]
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, "message":"Login successfull"}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"message":"Get url for the login page"},status=status.HTTP_200_OK)
