ae585f85078a9a781cb016b7a566cd7af7ce8287
Alex <mathewalex806@gmail.com>
2024-07-31 12:57:26 +0530

    added signup url

--- Solstice/app1/views.py
+++ Solstice/app1/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 
-
-def index(request):
-    return HttpResponse("This is the index page")
+@api_view(['POST',"GET"])
+def signup(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"message":"Get url for the signup url"},status=status.HTTP_200_OK)
