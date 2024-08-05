7fbbb790ffad43771998b365459ac139f27a440f
Alex <mathewalex806@gmail.com>
2024-08-05 21:52:11 +0530

    stable config

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensure Vite listens on all network interfaces
+    port: 5173 // This is the port inside the container
+  }
 })
