4be368582260a56d1684f1323ef098ed4473b6c8
Alex <mathewalex806@gmail.com>
2024-09-05 12:45:50 +0530

    adding views

--- Solstice/app1/views.py
+++ Solstice/app1/views.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSerializer
+from .serializers import UserSerializer, WatchlistCompanyserializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate, login
 from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
-from .models import Portfolio, Watchlist, Watchlist_company, Investment, Transactions, Portfolio_performance
+from .models import Portfolio, Watchlist, Watchlist_company, Investment, Transactions, Portfolio_performance, Company
 from django.contrib.auth.models import User
 
 @csrf_exempt
@@ -78,3 +78,46 @@ def create_watchlist(request):
     return Response({"message":"GET url for the create watchlist route"})
 
 
+# POST request to add company to watchlist
+# GET request to get all companies in watchlist
+
+@api_view(['POST', 'GET'])
+@permission_classes([IsAuthenticated])
+def watchlist(request):
+    if request.method == "POST":
+        user = request.user.username
+        comp_name = request.data.get("name")  
+        print(comp_name)
+        try:
+            watchlist = Watchlist.objects.get(user__username=user)
+            print(watchlist)
+            company = Company.objects.get(name=comp_name)
+            print(company)
+            comp_watchlist = Watchlist_company.objects.create(watchlist=watchlist, company=company)
+            print(comp_watchlist)
+
+            return Response({"message": f"Added {comp_name} to watchlist"}, status=status.HTTP_201_CREATED)
+        
+        except Watchlist.DoesNotExist:
+            return Response({"error": "Watchlist not found for user"}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Company.DoesNotExist:
+            return Response({"error": f"Company {comp_name} not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Exception as e:
+            print(e)  
+            return Response({"error": "Failed to add to watchlist"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "GET":
+        user = request.user.username
+        try:
+            watchlist = Watchlist.objects.get(user__username=user)
+            watchlist_comp = Watchlist_company.objects.filter(watchlist=watchlist)
+
+            serializer = WatchlistCompanyserializer(watchlist_comp, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Watchlist.DoesNotExist:
+            return Response({"error": "Watchlist not found for user"}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            print(e)  
+            return Response({"error": "Failed to get watchlist"}, status=status.HTTP_400_BAD_REQUEST)
